5fabc9793604b194c60987d99bc4a659f8002682
Tobias Hille <tobihille@googlemail.com>
2015-07-04 13:41:13 +0200

    implemented put and delete, improved apiTest, added groupBy and Limit in get

--- apiTest.php
+++ apiTest.php
@@ -1,10 +1,19 @@
 <?php
 
+$argv = $_GET;
+
 require_once('app/Mage.php');
 
 Mage::app('admin');
 
-$input = array(
+Mage::register('custom_entry_point', true, true);
+$apihost = Mage::helper('core/url')->getHomeUrl().'api/v2_soap?wsdl=1';
+
+$file = fopen( dirname(__FILE__).'/image.png', 'r');
+$imageData = base64_encode( fgets($file) );
+fclose($file);
+
+$select = array(
   'main_table' => 'sales/order',
   'join' => array(
     array(
@@ -26,9 +35,139 @@ $input = array(
     'sfoi.product_type' => array('eq' => 'simple'),
     'cpe.type_id' => array('nin' => array('bunde', 'configurable'))
   ),
-  'limit' => 42
+  'limit' => 42,
+  'group' => array(
+    'main_table.entity_id',
+    'cpe.sku'
+  )
 );
 
-$data = Mage::getModel('apimodelbridge/api')->fetchData( json_encode($input) );
+$putInsert = array(
+  'main_table' => 'catalog/product',
+    //note that you can not join other tables/models in insert or update-mode
+  'entity_data' => array(
+    array(
+      'sku' => 'abc0001', //please note that in case of catalog/product you don't even need an entity_id to update, but this is not usual
+      'type' => 'simple',
+      'attribute_set_id' => 4,
+      'small_image' => $imageData,
+      'thumbnail' => $imageData,
+      'visibility' => 4,
+      'price' => 19.99,
+      'name' => 'Sample Product',
+      'short_description' => 'foo',
+      'description' => 'bar',
+      'status' => 1,
+      'tax_class_id' => 0,
+      'qty' => 5,
+      'is_in_stock' => 1,
+      'website_ids' => array(1),
+    ),
+    array(
+      'sku' => 'abcdef001',
+      'type' => 'simple',
+      'attribute_set_id' => 4,
+      'small_image' => $imageData,
+      'thumbnail' => $imageData,
+      'visibility' => 1,
+      'price' => 42.23,
+      'name' => 'Sample Product 2',
+      'short_description' => 'fooX',
+      'description' => 'barX',
+      'status' => 2,
+      'tax_class_id' => 1,
+      'qty' => 1,
+      'is_in_stock' => 0,
+    )
+  ),
+);
 
+$putUpdate = $putInsert;
+//the only difference - magento finds the data by their id which is in most cases in entity_id
+$putUpdate['entity_data'][0]['entity_id'] = 1000;
+$putUpdate['entity_data'][1]['entity_id'] = 1042;
+
+$delete = array(
+  'main_table' => 'catalog/product',
+  'ids' => array(
+    '1000',
+    '1042'
+  )
+);
+
+$data = Mage::getModel('apimodelbridge/api')->putData( json_encode($putInsert) );
 echo $data;
+
+$delete['ids'] = json_decode($data);
+
+$data = Mage::getModel('apimodelbridge/api')->deleteData( json_encode($delete) );
+echo $data;
+
+die('ende');
+
+if ( $argv['mode'] == 'model')
+{
+  if ( $argv['method'] == 'get' )
+  {
+    $data = Mage::getModel('apimodelbridge/api')->fetchData( json_encode($select) );
+    echo $data;
+  }
+  elseif ($argv['method'] == 'put')
+  {
+    $data = Mage::getModel('apimodelbridge/api')->putData( json_encode($putInsert) );
+    echo $data;
+
+    $data = Mage::getModel('apimodelbridge/api')->putData( json_encode($putUpdate) );
+    echo $data;
+  }
+  elseif ($argv['method'] == 'delete')
+  {
+    $data = Mage::getModel('apimodelbridge/api')->deleteData( json_encode($delete) );
+    echo $data;
+  }
+  else
+  {
+    echo "Method not provided or incorrect, allowed: get, put, delete";
+  }
+}
+else //API
+{
+  $username = $argv["user"];
+  $password = $argv["pass"];
+
+  try
+  {
+    $soap = new SoapClient($apihost, array('cache_wsdl' => WSDL_CACHE_NONE));
+    $sess = $soap->login($username, $password);
+
+    if ( $argv['method'] == 'get' )
+    {
+      $result = $soap->apimodelbridgeGet($sess, json_encode($select) );
+      echo $result;
+    }
+    elseif ($argv['method'] == 'put')
+    {
+      $result = $soap->apimodelbridgePut($sess, json_encode($putInsert) );
+      echo $result;
+
+      $result = $soap->apimodelbridgePut($sess, json_encode($putUpdate) );
+      echo $result;
+    }
+    elseif ($argv['method'] == 'delete')
+    {
+      $result = $soap->apimodelbridgeDelete($sess, json_encode($delete) );
+      echo $result;
+    }
+    else
+    {
+      echo "Method not provided or incorrect, allowed: get, put, delete";
+    }
+
+    echo "\ncomplete\n";
+  }
+  catch (Exception $e)
+  {
+    unset($e->xdebug_message);
+    var_dump($e);
+  }
+}
--- app/code/community/Tobihille/ApiModelBridge/Model/Api.php
+++ app/code/community/Tobihille/ApiModelBridge/Model/Api.php
@@ -54,9 +54,13 @@ class Tobihille_ApiModelBridge_Model_Api extends Mage_Api_Model_Resource_Abstrac
         foreach ($condition as $mode => $value)
         {
           $collection->addFieldToFilter($field, array($mode => $value));
-          //$select->where( $field, $value, $mode );
         }
       }
+    }
+
+    if ( !empty($input->group) )
+    {
+      $select->group($input->group);
     }
 
     if ( !empty($input->limit) )
@@ -68,31 +72,91 @@ class Tobihille_ApiModelBridge_Model_Api extends Mage_Api_Model_Resource_Abstrac
         getConnection('core_read')->
         fetchAll( $collection->getSelectSql() );
 
-    return json_encode($data);
+    return json_encode( $data );
   }
 
   public function putData($json)
   {
-    $result = false;
+    $result = array();
 
     if ( !Mage::getStoreConfig('api/apimodelbridge/enable') )
     {
-      return $result;
+      return json_encode($result);
     }
 
-    return $result;
+    if ( !Mage::getStoreConfig('api/apimodelbridge/enable_put') )
+    {
+      return json_encode($result);
+    }
+
+    $input = json_decode($json);
+
+    $ids = array();
+
+    /**
+     * @var Mage_Core_Model_Abstract $model
+     */
+    $model = Mage::getModel($input->main_table);
+
+    foreach ($input->entity_data as $entity)
+    {
+      $model->clearInstance();
+
+      foreach ($entity as $key => $value)
+      {
+        $model->setData($key, $value);
+      }
+
+      $model->save();
+
+      $ids[] = $model->getId();
+    }
+
+    return json_encode($ids);
   }
 
   public function deleteData($json)
   {
-    $result = false;
+    $result = array();
 
     if ( !Mage::getStoreConfig('api/apimodelbridge/enable') )
     {
-      return $result;
+      return json_encode($result);
     }
 
-    return $result;
+    if ( !Mage::getStoreConfig('api/apimodelbridge/enable_delete') )
+    {
+      return json_encode($result);
+    }
+
+    $input = json_decode($json);
+
+    Mage::register('isSecureArea', true, true);
+
+    $result = array();
+
+    /**
+     * @var Mage_Core_Model_Abstract $model
+     */
+    $model = Mage::getModel($input->main_table);
+
+    foreach ($input->ids as $id)
+    {
+      try
+      {
+        $model->load($id);
+
+        $model->delete();
+
+        $result[] = true;
+      }
+      catch (Exception $e)
+      {
+        $result[] = false;
+      }
+    }
+
+    return json_encode($result);
   }
 
 }
